182c83e6372a5714ca498d6022092a88c1bbbe06
Armir Halimi <armirhalimi15@gmail.com>
2021-10-25 18:14:05 -0500

    fixes
    Done some fixes before uploading the assignment

--- NucampFolder/Python/1-Fundamentals/battlegame.py
+++ NucampFolder/Python/1-Fundamentals/battlegame.py
@@ -19,18 +19,21 @@
     #Write code so that when the game is over, the player is asked if they want to play again. (Hint: You can use a loop within a loop.)
 
 def BattleGame():
+    #characters
     wizard = "Wizard"
     elf = "Elf"
     human = "Human"
     orc = "Orc"
     end_game = "End Game"
 
+    #health points
     wizard_hp = 70
     elf_hp = 100
     human_hp = 200
     orc_hp = 90
     dragon_hp = 300
 
+    #damage points
     wizard_damage = 150
     elf_damage = 100
     human_damage = 20
@@ -42,56 +45,59 @@ def BattleGame():
     for i, value in enumerate(characters, 1):
         print("{}.) {}".format(i, value))
 
-    character = (int(input("Choose your character character:")))
+    character = input("Choose your character character:")
 
     while True:
-        if character == 1 or character == "wizard":
+        if character == "1" or character.lower == "wizard":
             character = wizard
             my_hp = wizard_hp
             my_damage = wizard_damage
             break
-        elif character == 2 or character == "elf":
+        elif character == "2" or character.lower == "elf":
             character = elf
             my_hp = elf_hp
             my_damage = elf_damage
             break
-        elif character == 3 or character == "human":
+        elif character == "3" or character.lower == "human":
             character = human
             my_hp = human_hp
             my_damage = human_damage
             break
-        elif character == 4 or character == "orc":
+        elif character == "4" or character.lower == "orc":
             character = orc
             my_hp = orc_hp
             my_damage = orc_damage
             break
-        elif character == 5 or character == "end game":
+        elif character == "5" or character.lower == "end game":
             quit()
         print("Unknown Character")
 
-    print("You have chosen the character: " + character)
+    print("You have chosen the character:", character)
     print("Health:", my_hp)
     print("Damage:", my_damage)
 
-    # game battle section
+    # task 4, battle with dragon
     while True:
         dragon_hp = dragon_hp - my_damage
-        print("The " + character + " has damaged the Dragon!")
+        print("The " , character , " has damaged the Dragon!\n")
         if dragon_hp > 0:
-            print("The dragon has ", dragon_hp ," left.")
+            print("The dragon has ", str(dragon_hp) ," left.")
         if dragon_hp <= 0:
-            print(f"The Dragon has lost the battle. {character} wins!")
+            print("The Dragon has lost the battle." ,character , " wins!\n")
             break
         my_hp = my_hp-dragon_damage
-        print(f"The Dragon damaged the {character}! You have {my_hp} left!")
+        print("The Dragon damaged the" ,character,"! You have ", str(my_hp) ," left!\n")
         if my_hp <= 0:
-            print("The " + character + " has lost the battle!")
+            print("The " , character , " has lost the battle!")
             break
 
-    play_again=input("Would you like to play again? (yes/no) :")
-    if play_again == "yes":
+def play_again():
+    response = input("Would you like to play again? (yes/no) :").lower()
+    if response == "yes":
         BattleGame()
     else:
         print("You have exited the game!")
 
 BattleGame()
+play_again()
+
